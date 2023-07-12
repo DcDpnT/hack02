9307fe018a86358c4510b273178a5acc31bdd591
Dagets <inard.frederic@gmail.com>
2023-07-12 23:06:26 +0200

    header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import "./Home.css";
+import Header from "../Header1";
+
 
 function Home() {
 
     return (
-        <p>Page Home</p>
+        <>
+        <Header/>
+        {/* <p>Page Home</p> */}
+        </>
     )
     }
     
-    export default Home
+export default Home
