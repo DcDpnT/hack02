830e8e712b7ccd3e27aff5b9c06d51fd072d0671
Vivien Floureck <vivienfloureck@outlook.com>
2023-07-12 18:41:39 +0200

    add all medecins with app.js

--- src/components/CardMedecin.jsx
+++ src/components/CardMedecin.jsx
@@ -1,14 +1,33 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
+import axios from "axios";
 import "./CardMedecin.scss"
 
-function CardMedecin() {
+  function CardMedecin() {
+		const [dataMedecin, setDataMedecin] = useState([]);
+
+  useEffect(() => {
+        axios.get("http://localhost:4242/api/medecins").then((response) => {
+          console.info(response.data);
+          setDataMedecin(response.data);
+        });
+      }, []);
+
 	return (
-		<div className="fondgris">
-			<div>
+		
+			<div className="wrap-container">
 				{/* <img src={medecin.img} alt="image du medecin"/> */}
-				<p>coucou</p>
-			{/* calendrier  */}
-			</div>
-		</div>
+				{dataMedecin
+				// .filter((medecin)=>
+				// ) 
+				.map((medecin)=> (	
+					<p className="fondgris"  key={medecin.id}>
+					{medecin.nom}<br/>{medecin.lieu} <Link to={`/medecin/${medecin.id}`}><button type="button"> prendre rendez-vous</ button> </Link></p>
+						
+				))}
+			</div>	
+	
+	
 
 	)
 }
