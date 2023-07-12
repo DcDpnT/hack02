7c598ef5f964e0289090d9483851aa162d75a523
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-07-12 17:46:54 +0200

    home

---
+++ src/components/TableauHome.jsx
@@ -0,0 +1,90 @@
+import Conjonctivite from "../assets/Congonctivite.png"
+import Diarrhée from "../assets/Diarrhée.png"
+import DouleursMusculaires from "../assets/DouleursMusculaires.png"
+import DouleursVentre from "../assets/DouleursVentre.png"
+import ErrupionCutanée from "../assets/ErrupionCutanée.png"
+import Ganglions from "../assets/Ganglions.png"
+import GrandeFatigue from "../assets/GrandeFatigue.png"
+import Maigre from "../assets/Maigre.png"
+import MalDents from "../assets/MalDents.png"
+import MalDeTete from "../assets/MalDeTete.png"
+import NezQuiCoule from "../assets/NezQuiCoule.png"
+import TachesNoirs from "../assets/TachesNoirs.png"
+import Toux from "../assets/Toux.png"
+import Vomissement from "../assets/Vomissemnet.png"
+
+const TableauHome = [
+    {
+      id: 1,
+      symptomes: "conjonctivite",
+      imgsrc: src={Conjonctivite}
+    },
+    {
+      id: 2,
+      symptomes: "diarrhée sanglante",
+      imgsrc: src={Diarrhée},
+    },
+    {
+      id: 3,
+      symptomes: "faiblesse musculaire",
+      imgsrc: src={DouleursMusculaires}
+    },
+    {
+      id: 4,
+      symptomes: "douleurs abdominales",
+      imgsrc: src={DouleursVentre},
+    },
+    {
+    id: 5,
+    symptomes: "éruptions cutanées",
+    imgsrc: src={ErrupionCutanée},
+    },
+      {
+        id: 6,
+        symptomes: "ganglions enflés",
+        imgsrc: src={Ganglions},
+      },
+      {
+        id: 7,
+        symptomes: "fatigue intense",
+        imgsrc: src={GrandeFatigue},
+      },
+      {
+        id: 8,
+        symptomes: "perte de poids",
+        imgsrc: src={Maigre},
+      },
+      {
+        id: 9,
+        symptomes: "mal aux dents",
+        imgsrc: src={MalDents},
+      },
+      {
+        id: 10,
+        symptomes: "maux de tête",
+        imgsrc: src={MalDeTete},
+      },
+      {
+        id: 11,
+        symptomes: "écoulement du nez",
+        imgsrc: src={NezQuiCoule},
+      },
+      {
+        id: 12,
+        symptomes: "éruptions cutanées",
+        imgsrc: src={TachesNoirs},
+      },
+      {
+        id: 13,
+        symptomes: "toux sèche",
+        imgsrc: src={Toux},
+      },
+      {
+        id: 14,
+        symptomes: "nausées",
+        imgsrc: src={Vomissement},
+      },
+
+  ];
+  
+  export default TableauHome;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,19 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import TableauHome from "./components/TableauHome";
 
 import "./Home.scss";
 
 function Home() {
-    const [home, setHome] = useState([]);
-
-    useEffect(() => {
-        axios.get("http://localhost:4242/api/symptomes").then((response) => {
-          console.info(response.data);
-          setHome(response.data);
-        });
-      }, []);
-
     return (
-        <p>Page Home</p>
-    )
-    }
+        <div>
+        {TableauHome.map((item) => (
+            <div key={item.id}>
+                <img src={item.imgSrc} alt={item.symptomes} />
+                <p>{item.symptomes}</p>
+            </div>
+        ))}
+    </div>
+);
+};
     
     export default Home
